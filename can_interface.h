1bb44bc6428c64e6e927bbd7d3c6eabef543ed21
Antoine Albertelli <antoinea101@gmail.com>
2014-10-22 11:22:14 +0200

    Add CAN interface and mocks

---
+++ can_interface.h
@@ -0,0 +1,23 @@
+#ifndef CAN_INTERFACE_H
+#define CAN_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** Reads a message from the CAN interface.
+ *
+ * @param [out] message_id A pointer to the variable where the message ID will be stored.
+ * @param [out] message A buffer where the CAN message will be stored. Should be large enough to contain 8 bytes.
+ * @returns The number of bytes in the message.
+ */
+int can_interface_read_message(uint32_t *message_id, uint8_t *message);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ tests/mocks/can_interface_mock.cpp
@@ -0,0 +1,70 @@
+#include "../../can_interface.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+#include <cstring>
+
+static uint32_t expected_id;
+uint8_t expected_msg[8];
+
+
+int can_interface_read_message(uint32_t *message_id, uint8_t *message)
+{
+    return mock("can").actualCall("read")
+                      .withOutputParameter("id", message_id)
+                      .withOutputParameter("message", message)
+                      .returnIntValue();
+}
+
+void can_mock_message(uint32_t message_id, uint8_t *msg, int message_len)
+{
+    expected_id = message_id;
+    memcpy(expected_msg, msg, message_len);
+
+    mock("can").expectOneCall("read")
+               .withOutputParameterReturning("id", &expected_id, sizeof(uint32_t))
+               .withOutputParameterReturning("message", expected_msg, message_len)
+               .andReturnValue(message_len);
+}
+
+TEST_GROUP(CanInterfaceMockTestGroup)
+{
+    uint32_t id;
+    uint8_t msg[8];
+
+    void teardown()
+    {
+        mock().checkExpectations();
+        mock().clear();
+    }
+
+};
+
+
+TEST(CanInterfaceMockTestGroup, CanGetNodeID)
+{
+    can_mock_message(42, msg, 0);
+
+    can_interface_read_message(&id, msg);
+
+    CHECK_EQUAL(42, id);
+}
+
+TEST(CanInterfaceMockTestGroup, CanGetMessageLength)
+{
+    int len;
+    can_mock_message(42, (uint8_t *)"hello", 5);
+
+    len = can_interface_read_message(&id, msg);
+    CHECK_EQUAL(5, len);
+}
+
+
+TEST(CanInterfaceMockTestGroup, CanGetMessage)
+{
+    int len;
+    can_mock_message(42, (uint8_t *)"hello", 5);
+
+    can_interface_read_message(&id, msg);
+    STRCMP_EQUAL("hello", (char *)msg);
+}
