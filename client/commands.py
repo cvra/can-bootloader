eb50b60e608b7657fdfeb58b8aa62236db24ae9d
Antoine Albertelli <antoinea101@gmail.com>
2014-12-12 13:54:41 +0100

    Refactoring

--- client/commands.py
+++ client/commands.py
@@ -4,18 +4,23 @@ class CommandType:
     JumpToMain = 1
     Write = 3
 
+def encode_command(command_code, *arguments):
+    """
+    Encodes a command of the given type with given arguments.
+    """
+    p = Packer(use_bin_type=True)
+    obj = list(arguments)
+    return p.pack(command_code) +  p.pack(obj)
+
 def encode_write_flash(data, adress, device_class):
     """
     Encodes the command to write the given data at the given adress in a
     messagepack byte object.
     """
-    p = Packer(use_bin_type=True)
-    obj = [adress, device_class, data]
-    return p.pack(CommandType.Write) +  p.pack(obj)
+    return encode_command(CommandType.Write, adress, device_class, data)
 
 def encode_jump_to_main():
     """
     Encodes the command to jump to application using MessagePack.
     """
-    p = Packer(use_bin_type=True)
-    return p.pack(CommandType.JumpToMain)
+    return encode_command(CommandType.JumpToMain)
