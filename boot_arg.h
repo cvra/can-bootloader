9b0b239a87befe618b1cb044bcc87664547870ac
Antoine Albertelli <antoinea101@gmail.com>
2014-12-22 11:21:55 +0100

    Add tests for boot to application

--- boot_arg.h
+++ boot_arg.h
@@ -1,6 +1,10 @@
 #ifndef BOOT_ARG_H
 #define BOOT_ARG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 #define BOOT_ARG_START_BOOTLOADER               0x00
@@ -9,5 +13,9 @@
 #define BOOT_ARG_START_ST_BOOTLOADER            0x03
 
 void reboot(uint8_t arg);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BOOT_ARG_H
