312df2c52e24e0971567d1c0dfde6432df2f020a
Michael Spieler <the.chaotic.coder@gmail.com>
2015-01-05 00:05:40 +0100

    Implement CAN interface, change API.

--- can_interface.h
+++ can_interface.h
@@ -6,20 +6,28 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 /** Reads a message from the CAN interface.
- *
- * @param [out] message_id A pointer to the variable where the message ID will be stored.
+ * @param [out] id A pointer to the variable where the message ID will be stored.
  * @param [out] message A buffer where the CAN message will be stored. Should be large enough to contain 8 bytes.
- * @returns The number of bytes in the message.
+ * @param [out] length A pointer to the message length.
+ * @param [in] retries The number of retries until timeout.
+ * @returns true if read was successful.
  */
-int can_interface_read_message(uint32_t *message_id, uint8_t *message);
+bool can_interface_read_message(uint32_t *id, uint8_t *message, uint8_t *length, uint32_t retries);
 
-void can_interface_send_message(uint32_t message_id, uint8_t *message, int len);
+/** Sends a message via the CAN interface.
+ * @param [in] id The CAN ID to address.
+ * @param [in] message The message data.
+ * @param [in] length The message length.
+ * @param [in] retries The number of retries until timeout.
+ * @returns true if write was successful.
+ */
+bool can_interface_send_message(uint32_t id, uint8_t *message, uint8_t length, uint32_t retries);
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif
