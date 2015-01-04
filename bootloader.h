78bca3a2f04b351ba7d7171a8c6ae54c58fada33
Michael Spieler <the.chaotic.coder@gmail.com>
2015-01-05 00:44:53 +0100

    Add bootloader main routine.

---
+++ bootloader.h
@@ -0,0 +1,15 @@
+
+#ifndef BOOTLOADER_H
+#define BOOTLOADER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bootloader_main(int arg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOOTLOADER_H */
