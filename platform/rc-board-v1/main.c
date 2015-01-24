ea28d54120a919569184f9e907f90c6855fa5101
Michael Spieler <the.chaotic.coder@gmail.com>
2015-01-25 00:03:19 +0100

    rc-board-v1: Configure for external oscillator clock.

--- platform/rc-board-v1/main.c
+++ platform/rc-board-v1/main.c
@@ -1,4 +1,5 @@
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/can.h>
 #include <stddef.h>
@@ -81,9 +82,75 @@ void fault_handler(void)
     reboot(BOOT_ARG_START_BOOTLOADER_NO_TIMEOUT);
 }
 
+typedef struct {
+    uint8_t pllpre;
+    uint8_t pll;
+    uint8_t pllsrc;
+    uint32_t flash_config;
+    uint8_t hpre;
+    uint8_t ppre1;
+    uint8_t ppre2;
+    uint8_t power_save;
+    uint32_t apb1_frequency;
+    uint32_t apb2_frequency;
+} my_clock_scale_t;
+
+// clock config for external HSE 16MHz cristal
+static const my_clock_scale_t clock_72mhz = {
+    .pllpre = RCC_CFGR2_PREDIV_HSE_IN_PLL_DIV_2,
+    .pll = RCC_CFGR_PLLMUL_PLL_IN_CLK_X9,
+    .pllsrc = RCC_CFGR_PLLSRC_HSE_PREDIV,
+    .hpre = RCC_CFGR_HPRE_DIV_NONE,
+    .ppre1 = RCC_CFGR_PPRE1_DIV_2,
+    .ppre2 = RCC_CFGR_PPRE2_DIV_NONE,
+    .power_save = 1,
+    .flash_config = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_2WS,
+    .apb1_frequency = 36000000,
+    .apb2_frequency = 72000000,
+};
+
+static inline void rcc_set_main_pll_hse(uint32_t pll)
+{
+    RCC_CFGR = (~RCC_CFGR_PLLMUL_MASK & RCC_CFGR) |
+        (pll << RCC_CFGR_PLLMUL_SHIFT) | RCC_CFGR_PLLSRC;
+}
+
+static inline void rcc_clock_setup_hse(const my_clock_scale_t *clock)
+{
+    /* Enable internal high-speed oscillator. */
+    rcc_osc_on(HSE);
+    rcc_wait_for_osc_ready(HSE);
+    /* Select HSE as SYSCLK source. */
+    rcc_set_sysclk_source(RCC_CFGR_SW_HSE);
+    rcc_wait_for_sysclk_status(HSE);
+
+    rcc_osc_off(PLL);
+    rcc_wait_for_osc_not_ready(PLL);
+    rcc_set_pll_source(clock->pllsrc);
+    rcc_set_main_pll_hse(clock->pll);
+    RCC_CFGR2 = (clock->pllpre << RCC_CFGR2_PREDIV_SHIFT);
+    /* Enable PLL oscillator and wait for it to stabilize. */
+    rcc_osc_on(PLL);
+    rcc_wait_for_osc_ready(PLL);
+
+    rcc_set_hpre(clock->hpre);
+    rcc_set_ppre2(clock->ppre2);
+    rcc_set_ppre1(clock->ppre1);
+    /* Configure flash settings. */
+    flash_set_ws(clock->flash_config);
+    /* Select PLL as SYSCLK source. */
+    rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
+    /* Wait for PLL clock to be selected. */
+    rcc_wait_for_sysclk_status(PLL);
+
+    /* Set the peripheral clock frequencies used. */
+    rcc_apb1_frequency = clock->apb1_frequency;
+    rcc_apb2_frequency = clock->apb2_frequency;
+}
+
 void platform_main(int arg)
 {
-    rcc_clock_setup_hsi(&hsi_8mhz[CLOCK_64MHZ]);
+    rcc_clock_setup_hse(&clock_72mhz);
 
     // LEDs
     rcc_periph_clock_enable(RCC_GPIOC);
