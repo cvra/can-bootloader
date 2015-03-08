328d1ce56db7d331b1606713fdd09948c19dd512
Michael Spieler <the.chaotic.coder@gmail.com>
2015-03-08 19:24:15 +0100

    Fix unit tests.

--- tests/config_commands_tests.cpp
+++ tests/config_commands_tests.cpp
@@ -1,7 +1,7 @@
 #include <cstring>
 #include <CppUTest/TestHarness.h>
 #include <CppUTestExt/MockSupport.h>
-#include <serializer/serialization.h>
+#include <cmp_mem_access/cmp_mem_access.h>
 
 #include "../flash_writer.h"
 #include "../command.h"
@@ -9,37 +9,30 @@
 
 TEST_GROUP(ConfigCommandTestGroup)
 {
-    serializer_t serializer;
-    cmp_ctx_t command_builder;
-    char command_data[1024];
-
-    serializer_t out_serializer;
-    cmp_ctx_t output_ctx;
-    char output_data[1024];
-
+    cmp_mem_access_t write_cma;
+    cmp_mem_access_t read_cma;
+    cmp_ctx_t write_ctx;
+    cmp_ctx_t read_ctx;
+    char data[1024];
     bootloader_config_t config;
 
     void setup()
     {
-        serializer_init(&serializer, command_data, sizeof command_data);
-        serializer_cmp_ctx_factory(&command_builder, &serializer);
-        memset(command_data, 0, sizeof command_data);
+        cmp_mem_access_init(&write_ctx, &write_cma, data, sizeof data);
+        cmp_mem_access_ro_init(&read_ctx, &read_cma, data, sizeof data);
+        memset(data, 0, sizeof data);
         memset(&config, 0, sizeof config);
-
-        serializer_init(&out_serializer, output_data, sizeof output_data);
-        serializer_cmp_ctx_factory(&output_ctx, &out_serializer);
-        memset(output_data, 0, sizeof output_data);
     }
 };
 
 TEST(ConfigCommandTestGroup, CanChangeNodeID)
 {
-    cmp_write_map(&command_builder, 1);
+    cmp_write_map(&write_ctx, 1);
 
-    cmp_write_str(&command_builder, "ID", 2);
-    cmp_write_u8(&command_builder, 42);
+    cmp_write_str(&write_ctx, "ID", 2);
+    cmp_write_u8(&write_ctx, 42);
 
-    command_config_update(1, &command_builder, NULL, &config);
+    command_config_update(1, &read_ctx, NULL, &config);
 
     CHECK_EQUAL(42, config.ID);
 }
@@ -50,8 +43,8 @@ TEST(ConfigCommandTestGroup, CanReadConfig)
     memset(&read_config, 0, sizeof(read_config));
 
     config.ID = 42;
-    command_config_read(0, NULL, &output_ctx, &config);
-    config_update_from_serialized(&read_config, &output_ctx);
+    command_config_read(0, NULL, &write_ctx, &config);
+    config_update_from_serialized(&read_config, &read_ctx);
 
     // Just check that the first field matches.
     // The rest is tested in ConfigSerializationTest
