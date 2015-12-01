98e00d9b25781de2cc168e31bc60c31cd56c197c
Michael Spieler <michael.spieler@epfl.ch>
2015-12-01 23:06:41 +0100

    Implement CVRA CAN dongle adapter

--- client/can/adapters.py
+++ client/can/adapters.py
@@ -1,6 +1,8 @@
 import can
 import socket
 import struct
+import serial
+from queue import Queue
 
 class SocketCANConnection:
     # See <linux/can.h> for format
@@ -34,3 +36,58 @@ class SocketCANConnection:
 
         return can.Frame(id=can_id, data=data[:can_dlc])
 
+class CVRACANDongleConnection:
+    """
+    Implements the CAN API for the CVRA CAN dongle.
+    """
+    def __init__(self, port, baudrate=115200, timeout=None):
+        import cvra_can
+        self.cvra_can = cvra_can
+        from datagrammessages import SerialConnection
+
+        self.conn = SerialConnection(port)
+        self.conn.set_msg_handler('rx', lambda msg: self.rx_handler(msg))
+        self.conn.set_msg_handler('drop', self.drop_handler)
+
+        id_filter = [int(self.cvra_can.Frame.ID(0, extended=0)),
+                     self.cvra_can.Frame.ID.mask(0, extended=1)]
+        if not self.conn.service_call('filter', id_filter):
+            print('filter configuration error')
+            sys.exit(1)
+        self.conn.service_call('silent', False)
+        self.conn.service_call('loop back', False)
+        self.conn.service_call('bit rate', 1000000)
+        self.rx_queue = Queue()
+
+    def rx_handler(self, msg):
+        rec = self.cvra_can.Frame.decode(msg[0])
+        if rec.can_id.extended:
+            return
+        frame = can.Frame(id=rec.can_id.value,
+                                 data=rec.data,
+                                 extended=rec.can_id.extended,
+                                 transmission_request=rec.can_id.remote,
+                                 data_length=len(rec.data))
+        self.rx_queue.put(frame)
+
+    def drop_handler(self, msg):
+        # non fatal error
+        # print a warning?
+        pass
+
+    def send_frame(self, frame):
+        ident = self.cvra_can.Frame.ID(value=frame.id,
+                                  extended=frame.extended,
+                                  remote=frame.transmission_request)
+        if frame.transmission_request:
+            data = frame.data_length * b'0'
+        else:
+            data = frame.data
+        frame = self.cvra_can.Frame(can_id=ident, data=data).encode()
+        self.conn.service_call('tx', [frame])
+
+    def receive_frame(self):
+        try:
+            return self.rx_queue.get(True, 1) # block with timeout 1 sec
+        except:
+            return None
