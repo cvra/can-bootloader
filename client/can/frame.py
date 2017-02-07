97ab6b7ff1c2081420805aa3fd5fccb4e8af5710
Michael Spieler <michael.spieler@epfl.ch>
2017-02-07 03:34:36 +0100

    printable frames

--- client/can/frame.py
+++ client/can/frame.py
@@ -26,3 +26,14 @@ class Frame:
 
     def __eq__(self, other):
         return self.id == other.id and self.data == other.data
+
+    def __str__(self):
+        if self.extended:
+            frame = '{:08X}'.format(self.id)
+        else:
+            frame = '{:03X}'.format(self.id)
+        frame += ' [{}]'.format(self.data_length)
+        if not self.transmission_request:
+            for b in self.data:
+                frame += ' {:02X}'.format(int(b));
+        return frame
