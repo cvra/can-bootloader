86bcf700ffe510f50a60b1a0b15a0cccc4a7a391
Antoine Albertelli <antoine@antoinealb.net>
2017-02-08 13:13:26 +0100

    Implement end of flash API for rc-board-v1

--- platform/rc-board-v1/platform.h
+++ platform/rc-board-v1/platform.h
@@ -15,11 +15,16 @@ extern "C" {
 extern uint8_t config_page_buffer[CONFIG_PAGE_SIZE];
 
 // symbols defined in linkerscript
-extern int application_address, config_page1, config_page2;
+extern int application_address, application_size, config_page1, config_page2;
 
 static inline void *memory_get_app_addr(void)
 {
     return (void *) &application_address;
+}
+
+static inline size_t memory_get_app_size(void)
+{
+    return (size_t)&application_size;
 }
 
 static inline void *memory_get_config1_addr(void)
